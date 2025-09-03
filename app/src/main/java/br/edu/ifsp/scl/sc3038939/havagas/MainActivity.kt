53708bbbee3dcb4f66fbd3a035a693bd1910c4ab
Aislan <aislan.pepi@outlook.com>
2025-09-03 19:53:05 -0300

    fix:layoutInflater

--- app/src/main/java/br/edu/ifsp/scl/sc3038939/havagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038939/havagas/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflate     r)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.cbTel.setOnClickListener { showMobilePhone() }
         binding.submit
